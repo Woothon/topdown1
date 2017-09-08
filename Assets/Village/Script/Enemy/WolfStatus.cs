87563dd9fbb7c09caf5dba0aff3fb8d5b470828a
Xiong.Wu <Xiong.Wu>
2017-09-08 17:37:24 +0800

    Add Wolfstatus

---
+++ Assets/Village/Script/Enemy/WolfStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfStatus : MonoBehaviour {
+
+	public string enemyName;
+    public int enemyId;
+    public Attribute status;
+    public int expGive;
+
+    [System.Serializable]
+    public class Attribute
+    {
+        public int hp, mp, atk, def, spd, hit;
+        public float criticalRate, atkSpd, atkRange, moveSpd;
+    }
+}
