774e30b75f5e6a796be44163d6cdc10b8dceaa48
woothon <woothon@163.com>
2017-09-19 17:28:29 +0800

    Add WooEnemyController and DropItem

--- Assets/Village/Script/Enemy/AnimationManagerWolf.cs
+++ Assets/Village/Script/Enemy/AnimationManagerWolf.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimationManagerWolf : MonoBehaviour {
+    public delegate void AnimationHandle();
+    public AnimationHandle animationState;
 
     [System.Serializable]
     public class AnimationType1
@@ -50,7 +52,7 @@ public class AnimationManagerWolf : MonoBehaviour {
     }
 
     [System.Serializable]
-    public class TakeDamage
+    public class AnimationTakeAttack
     {
         public string takeAttackName;
         public AnimationClip animation;
@@ -59,19 +61,134 @@ public class AnimationManagerWolf : MonoBehaviour {
 
     public AnimationType1 idle, death;
     public AnimationType2 move;
-    public List<NormalAttackAnimation> normalAttackAnimation;
-    public List<CriticalAttackAnimation> criticalAttackAnimation;
-    public List<TakeDamage> takeDamage;
+    public List<NormalAttackAnimation> normalAttack;
+    public List<CriticalAttackAnimation> criticalAttack;
+    public List<AnimationTakeAttack> takeAttack;
 
-
+    private WooEnemyController enemyController;
+    private WolfStatus enemyStatus;
+    [HideInInspector]
+    public bool checkAttack;
 
     // Use this for initialization
     void Start () {
-		
+        enemyController = GetComponent<WooEnemyController>();
+        enemyStatus = GetComponent<WolfStatus>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(animationState != null){
+            animationState();
+        }
 	}
+
+    public void Idle()
+    {
+        GetComponent<Animation>().CrossFade(idle.Animation.name);
+        GetComponent<Animation>()[idle.Animation.name].speed = idle.speedAnimation;
+    }
+
+    public void Move()
+    {
+        GetComponent<Animation>().Play(move.Animation.name);
+
+        if (move.speedTuning)
+        {
+            GetComponent<Animation>()[move.Animation.name].speed = (enemyStatus.status.moveSpd / 3f) / move.speedAnimation;
+        }
+        else
+        {
+            GetComponent<Animation>()[move.Animation.name].speed = move.speedAnimation;
+        }
+    }
+
+    public void Attack()
+    {
+        GetComponent<Animation>().Play(normalAttack[enemyController.typeAttack].animation.name);
+
+        if (normalAttack[enemyController.typeAttack].speedTuning)  //Enable Speed Tuning
+        {
+            GetComponent<Animation>()[normalAttack[enemyController.typeAttack].animation.name].speed = (enemyStatus.status.atkSpd / 100f) / normalAttack[enemyController.typeAttack].speedAnimation;
+        }
+        else
+        {
+            GetComponent<Animation>()[normalAttack[enemyController.typeAttack].animation.name].speed = normalAttack[enemyController.typeAttack].speedAnimation;
+        }
+
+        //Calculate Attack
+        if (GetComponent<Animation>()[normalAttack[enemyController.typeAttack].animation.name].normalizedTime > normalAttack[enemyController.typeAttack].attackTimer && !checkAttack)
+        {
+            //Attack Damage
+            HeroController enemy;
+            enemy = enemyController.target.GetComponent<HeroController>();
+            enemy.GetDamage(enemyStatus.status.atk * normalAttack[enemyController.typeAttack].multipleDamage, enemyStatus.status.hit, normalAttack[enemyController.typeAttack].flinchValue
+                            , normalAttack[enemyController.typeAttack].attackEffect, normalAttack[enemyController.typeAttack].soundEffect);
+            checkAttack = true;
+        }
+
+        if (GetComponent<Animation>()[normalAttack[enemyController.typeAttack].animation.name].normalizedTime > 0.9f)
+        {
+            enemyController.ctrlAnimState = WooEnemyController.ControlAnimationState.WaitAttack;
+            checkAttack = false;
+        }
+    }
+
+    public void CriticalAttack()
+    {
+        GetComponent<Animation>().Play(criticalAttack[enemyController.typeAttack].animation.name);
+
+        if (criticalAttack[enemyController.typeAttack].speedTuning)  //Enable Speed Tuning
+        {
+            GetComponent<Animation>()[criticalAttack[enemyController.typeAttack].animation.name].speed = (enemyStatus.status.atkSpd / 100f) / criticalAttack[enemyController.typeAttack].speedAnimation;
+        }
+        else
+        {
+            GetComponent<Animation>()[criticalAttack[enemyController.typeAttack].animation.name].speed = criticalAttack[enemyController.typeAttack].speedAnimation;
+        }
+
+        //Calculate Attack
+        if (GetComponent<Animation>()[criticalAttack[enemyController.typeAttack].animation.name].normalizedTime > criticalAttack[enemyController.typeAttack].attackTimer && !checkAttack)
+        {
+            //Attack Damage
+            HeroController enemy;
+            enemy = enemyController.target.GetComponent<HeroController>();
+
+            enemy.GetDamage(enemyStatus.status.atk * criticalAttack[enemyController.typeAttack].multipleDamage, 10000, criticalAttack[enemyController.typeAttack].flinchValue
+                            , criticalAttack[enemyController.typeAttack].attackEffect, criticalAttack[enemyController.typeAttack].soundEffect);
+
+            checkAttack = true;
+        }
+
+        if (GetComponent<Animation>()[criticalAttack[enemyController.typeAttack].animation.name].normalizedTime > 0.9f)
+        {
+            enemyController.ctrlAnimState = WooEnemyController.ControlAnimationState.WaitAttack;
+            checkAttack = false;
+        }
+    }
+
+    public void TakeAttack()
+    {
+        GetComponent<Animation>().CrossFade(takeAttack[enemyController.typeTakeAttack].animation.name);
+        GetComponent<Animation>()[takeAttack[enemyController.typeTakeAttack].animation.name].speed = takeAttack[enemyController.typeTakeAttack].speedAnimation;
+
+        if (GetComponent<Animation>()[takeAttack[enemyController.typeTakeAttack].animation.name].normalizedTime > 0.9f)
+        {
+            enemyController.ctrlAnimState = WooEnemyController.ControlAnimationState.WaitAttack;
+        }
+    }
+
+    public void Death()
+    {
+        GetComponent<Animation>().CrossFade(death.Animation.name);
+        GetComponent<Animation>()[death.Animation.name].speed = death.speedAnimation;
+
+        if (GetComponent<Animation>()[death.Animation.name].normalizedTime > 0.9f)
+        {
+            if (enemyController.deadTransparent)
+            {
+                enemyController.DeadTransparentSetup();
+            }
+        }
+    }
 }
--- Assets/Village/Script/Enemy/WooEnemyController.cs
+++ Assets/Village/Script/Enemy/WooEnemyController.cs
@@ -2,15 +2,250 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+[RequireComponent(typeof(AnimationManagerWolf))]
+[RequireComponent(typeof(WolfStatus))]
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(WooDropItem))]
+public class WooEnemyController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public enum ControlAnimationState { Idle, Move, WaitAttack, Attack, TakeAtk, Death };
+    public enum EnemyBehaviour { Standing, MoveAround }
+    public enum EnemyNature { Natural, Wild }
+    public enum MoveAroundBehavior { MoveToNext, Waiting }
+
+    public EnemyBehaviour behaviour;
+    public EnemyNature nature;
+    public MoveAroundBehavior moveBehavior;
+
+    public GameObject target;
+    public bool chaseTarget;
+    public List<GameObject> modelMesh = new List<GameObject>();
+
+    public Color colorTakeDamage;
+    public float deadTimer;
+    public bool deadTransparent;
+    public float speedFade;
+    public bool regenHP;
+    public bool regenMP;
+
+    public ControlAnimationState ctrlAnimState;
+
+    public float movePhase;
+    public float returnPhase;
+    public float delayNextTargetMin;
+    public float delayNextTargetMax;
+
+    private AnimationManagerWolf animationManager;
+    private WolfStatus enemyStatus;
+    private Quest_Data questData;
+    private CharacterController controller;
+
+    private float defaultReturnPhase;
+    private Vector3 spawnPoint;
+    private float timeToWaitBeforeNextMove;
+    private float deplayAttack = 100;
+    private Vector3 destinationPosition;
+    private float destinationDistance;
+    private Vector3 moveDir;
+    private float moveSpeed;
+    private Vector3 ctargetPos;
+    private Vector3 targetPos;
+    private Quaternion targetRotation;
+    private bool checkCritical;
+    private float flinchValue = 100;
+    private Vector3 randomMoveVector;
+    private float randomAngle;
+    private Shader alphaShader;
+    private Color[] defaultColor;
+    private bool enableRegen;
+    private GameObject detectArea;
+
+    private float fadeValue;
+    [HideInInspector]
+    public float defaultHP, defaultMP;
+    [HideInInspector]
+    public int typeAttack;
+    [HideInInspector]
+    public int typeTakeAttack;
+    [HideInInspector]
+    public bool startFade;
+
+    [HideInInspector]
+    public int sizeMesh;
+    // Use this for initialization
+    void Start () {
+        //set spawn point
+        destinationPosition = this.transform.position;
+
+        //get other component
+        animationManager = this.GetComponent<AnimationManagerWolf>();
+        enemyStatus = this.GetComponent<WolfStatus>();
+        controller = this.GetComponent<CharacterController>();
+
+        deplayAttack = 100;
+        flinchValue = 100;
+        fadeValue = 1;
+
+        spawnPoint = transform.position;
+
+        defaultReturnPhase = returnPhase;
+        defaultHP = enemyStatus.status.hp;
+        defaultMP = enemyStatus.status.mp;
+
+        defaultColor = new Color[modelMesh.Count];
+        
+        if(behaviour == EnemyBehaviour.MoveAround)
+        {
+            moveBehavior = MoveAroundBehavior.Waiting;
+        }
+
+        if(nature == EnemyNature.Wild)
+        {
+            detectArea = GameObject.Find("DetectArea");
+
+            if(detectArea == null)
+            {
+                Debug.LogWarning("Can't found DetectArea in Enemy -" + enemyStatus.name);
+            }
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        EnemyAnimationState();
+
+        if(ctrlAnimState != ControlAnimationState.Death)
+        {
+            if(behaviour == EnemyBehaviour.MoveAround && !chaseTarget && !target)
+            {
+                if(moveBehavior == MoveAroundBehavior.MoveToNext)
+                {
+                    UpdateMovingToNextPatrolPoint();
+                }else if(moveBehavior == MoveAroundBehavior.Waiting)
+                {
+                    UpdateWaitingforNextMove();
+                }
+            }
+        }
+        else
+        {
+            EnemyMovementBattle();
+        }
+    }
+
+    //State Enemy
+    void EnemyAnimationState()
+    {
+        if(ctrlAnimState == ControlAnimationState.Idle)
+        {
+            animationManager.animationState = animationManager.Idle;
+        }
+
+        if(ctrlAnimState == ControlAnimationState.Move)
+        {
+            animationManager.animationState = animationManager.Move;
+        }
+
+        if(ctrlAnimState == ControlAnimationState.WaitAttack)
+        {
+            animationManager.animationState = animationManager.Idle;
+            WaitAttack();
+        }
+
+        if (ctrlAnimState == ControlAnimationState.Attack)
+        {
+            LookAtTarget(target.transform.position);
+
+            if (checkCritical)
+            {
+                animationManager.animationState = animationManager.CriticalAttack;
+                deplayAttack = 100;
+            }
+            else
+            {
+                animationManager.animationState = animationManager.Attack;
+                deplayAttack = 100;
+            }
+        }
+
+        if(ctrlAnimState == ControlAnimationState.TakeAtk)
+        {
+            animationManager.animationState = animationManager.TakeAttack;
+        }
+
+        if(ctrlAnimState == ControlAnimationState.Death)
+        {
+            if(this.gameObject.tag == "Enemy")
+            {
+                this.gameObject.tag = "Untagged";
+                this.GetComponent<DropItem>().UseDropItem();
+            }
+
+            animationManager.animationState = animationManager.Death;
+
+            if (startFade)
+            {
+                DeadTransparentAlpha(speedFade);
+            }
+        }
+    }
+
+    void RandomPostion()
+    {
+        randomAngle = Random.Range(0f, 91);
+        randomMoveVector.x = Mathf.Sin(randomAngle) * movePhase + spawnPoint.x;
+        randomMoveVector.z = Mathf.Cos(randomAngle) * movePhase + spawnPoint.z;
+        randomMoveVector.y = transform.position.y;
+
+        targetRotation = Quaternion.LookRotation(randomMoveVector - transform.position);
+        destinationPosition = randomMoveVector;
+    }
+
+    void UpdateMovingToNextPatrolPoint()
+    {
+        destinationDistance = Vector3.Distance(destinationPosition, this.transform.position);
+
+        if(destinationDistance < 1f)
+        {
+            timeToWaitBeforeNextMove = Random.Range(delayNextTargetMin, delayNextTargetMax);
+            moveBehavior = MoveAroundBehavior.Waiting;
+            ctrlAnimState = ControlAnimationState.Idle;
+            moveSpeed = 0;
+        }
+
+        if(ctrlAnimState == ControlAnimationState.Move)
+        {
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, Time.deltaTime * 25);
+
+            if (controller.isGrounded)
+            {
+                moveDir = Vector3.zero;
+                moveDir = transform.TransformDirection(Vector3.forward* moveSpeed);
+            }
+        }
+        else
+        {
+            moveDir = Vector3.Lerp(moveDir, Vector3.zero, Time.deltaTime * 10);
+
+        }
+
+        moveDir.y -= 20 * Time.deltaTime;
+        controller.Move(moveDir* Time.deltaTime);
+    }
+
+    public void DeadTransparentSetup()
+    {
+        int index = 0;
+
+        alphaShader = Shader.Find("Transparent/Diffuse");
+
+        while (index < modelMesh.Count)
+        {
+            modelMesh[index].GetComponent<Renderer>().material.shader = alphaShader;
+            index++;
+        }
+
+        startFade = true;
+        deadTransparent = false;
+    }
 }
